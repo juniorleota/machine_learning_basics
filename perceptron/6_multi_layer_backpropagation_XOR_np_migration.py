ef9bc84244f357663e7dae89378440090cf85901
juniorleota <juniorleota040@gmail.com>
2024-04-10 19:25:36 +0900

    Add outline of class that will use np matrix/vector operations

---
+++ perceptron/6_multi_layer_backpropagation_XOR_np_migration.py
@@ -0,0 +1,31 @@
+'''
+This class will explore using np to simplify alot of the calculations done for MLP.
+'''
+import numpy as np
+import math
+
+def sigmoid(x):
+    pass
+
+def sigmoid_d(x):
+    pass
+
+def cross_entropy_loss(predicted_ouput, expected_output):
+    pass
+
+def cross_entropy_loss_d(predicted_ouput, expected_output):
+    pass
+
+class MLP:
+    def __init__(self, epochs=1000, learning_rate=0.1):
+        self.epochs = epochs
+        self.lr = learning_rate
+    
+    def forw_pass(self):
+        pass
+                  
+    def back_pass(self):
+        pass
+
+    def train(self, training_data, labels):
+        pass
