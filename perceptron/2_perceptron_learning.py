c4be6d4b9d693b087c2c85683b05fea1d50b1dfa
juniorleota <juniorleota040@gmail.com>
2024-04-10 17:53:08 +0900

    Remove unrelated code and use consistent formatting

--- perceptron/2_perceptron_learning.py
+++ perceptron/2_perceptron_learning.py
@@ -1,6 +1,6 @@
 class Perceptron:
     def __init__(self, training_iterations, learning_rate, threshold=0):
-        self.weights = [10, 10] 
+        self.weights = [10, 10]
         self.bias = 10
         self.learning_rate = learning_rate
         self.training_iterations = training_iterations
@@ -9,23 +9,22 @@ class Perceptron:
     def activation(self, x):
         if x >= self.threshold:
             return 1
-        return 0 
+        return 0
 
     def weighted_sum(self, a, b):
         res = 0
-        for x,y in zip(a, b):
-            res += x*y
+        for x, y in zip(a, b):
+            res += x * y
         return res
 
     def predict(self, feature):
         linear_output = self.weighted_sum(feature, self.weights) + self.bias
         return self.activation(linear_output)
 
-
     def train(self, features, labels):
         for epoch in range(self.training_iterations):
             # input vector has 2 values
-            all_pass = True 
+            all_pass = True
             for input_vector, y_expected in zip(features, labels):
                 y_prediction = self.predict(input_vector)
                 new_weights = []
@@ -38,13 +37,12 @@ class Perceptron:
                 self.weights = new_weights
                 self.bias = new_bias
             print(f"Epoch {epoch}: weights({self.weights}), bias({self.bias})")
-            if(all_pass):
+            if all_pass:
                 print("All training data passed")
                 break
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     training_data = [[0, 0], [0, 1], [1, 0], [1, 1]]
     label = [0, 0, 0, 1]
     perceptron = Perceptron(100, 0.09, 0)
