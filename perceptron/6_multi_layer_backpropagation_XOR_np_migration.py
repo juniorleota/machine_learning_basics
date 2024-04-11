63993afd22d7c129cd84eb3e4e8904edcdf3d0da
juniorleota <juniorleota040@gmail.com>
2024-04-11 17:11:28 +0900

    Use numpy to init mlp variables

--- perceptron/6_multi_layer_backpropagation_XOR_np_migration.py
+++ perceptron/6_multi_layer_backpropagation_XOR_np_migration.py
@@ -20,6 +20,12 @@ class MLP:
     def __init__(self, epochs=1000, learning_rate=0.1):
         self.epochs = epochs
         self.lr = learning_rate
+        # row is hidden neuron
+        # col is input
+        self.w_input_to_hidden = np.array([[0.1, 0.2],[0.3, 0.4]])
+        self.b_hidden = np.zeros(2)
+        self.w_hidden_to_output = np.array([0.25, 0.45])
+        self.b_output = np.zeros(1)
     
     def forw_pass(self):
         pass
@@ -29,3 +35,7 @@ class MLP:
 
     def train(self, training_data, labels):
         pass
+
+
+if __name__ == "__main__":
+    mlp = MLP()
