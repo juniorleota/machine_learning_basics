635d0769483d12a6233909652a86a47665dda925
juniorleota <juniorleota040@gmail.com>
2024-04-13 02:19:08 +0900

    Fix all known bugs

--- perceptron/6_multi_layer_backpropagation_XOR_np_migration.py
+++ perceptron/6_multi_layer_backpropagation_XOR_np_migration.py
@@ -24,6 +24,7 @@ def cross_entropy_loss(predicted_output, expected_output):
         + (1 - expected_output) * np.log(1 - predicted_output)
     )
 
+
 def cross_entropy_loss_derivative(predicted_output, expected_output):
     return -(expected_output / predicted_output) + (
         (1 - expected_output) / (1 - predicted_output)
@@ -38,7 +39,7 @@ class MLP:
         self.w_input_to_hidden = np.array([[0.1, 0.2], [0.3, 0.4]])
         self.b_hidden = np.zeros(2)
         # row is output neuron, col is hidden neuron
-        self.w_hidden_to_output = np.array([0.25, 0.45])
+        self.w_hidden_to_output = np.array([[0.25], [0.45]])
         self.b_output = np.zeros(1)
 
     def forw_pass(self, training_data):
@@ -73,22 +74,21 @@ class MLP:
         # note: the order here matters
         dzo_dwh2o = np.dot(hidden_activation.T, dao_dzo)
         # change in ouput output wrt output bias, the derivative is 1
-        dzo_dbo = (
-            dao_dzo * 1
-        )  # need to figure out why this is usually np.sum(dZ2, axis=0)
+        dzo_dbo = np.sum(dao_dzo, axis=0)
+        # need to figure out why this is usually np.sum(dZ2, axis=0)
         # chagne in ouput_output wrt to activation
-        # z = w*a(l-1) + b => dz/da = w * a(l-1)^-1 = w
-        dzo_dah = np.dot(dao_dzo,self.w_input_to_hidden.T)
-        # change in hidden activation wrt to hidden output 
-        dah_dzh = np.dot(dzo_dah, sigmoid_d(hidden_output))
-        # change in hidden output wrt to hidden weights 
+        # z = w*a(l-1) + b => dz/da = w * a(l-1)^0 = w
+        dzo_dah = np.dot(dao_dzo, self.w_hidden_to_output.T)
+        # change in hidden activation wrt to hidden output
+        dah_dzh = dzo_dah * sigmoid_d(hidden_output)
+        # change in hidden output wrt to hidden weights
         # z = w * Input + b
         # dz/dw = Input
-        dzh_dwi2h = np.dot(X.T, dah_dzh)
-        # change in hidden output wrt to hidden bias 
+        dzh_dwi2h = np.dot(training_data.T, dah_dzh)
+        # change in hidden output wrt to hidden bias
         # z = w * I + b
         # dz/db = 1
-        dzh_dbh = dah_dzh
+        dzh_dbh = np.sum(dah_dzh, axis=0)
         return dzo_dwh2o, dzo_dbo, dzh_dwi2h, dzh_dbh
 
     def train(self, training_data, labels):
@@ -96,27 +96,31 @@ class MLP:
             hidden_output, hidden_activation, output_output, output_activation = (
                 self.forw_pass(training_data)
             )
-            d_w_hidden_2_output, d_b_output, d_w_input_2_hidden, d_b_hidden = self.back_pass(
-                training_data,
-                labels,
-                hidden_output,
-                hidden_activation,
-                output_output,
-                output_activation,
+            d_w_hidden_2_output, d_b_output, d_w_input_2_hidden, d_b_hidden = (
+                self.back_pass(
+                    training_data,
+                    labels,
+                    hidden_output,
+                    hidden_activation,
+                    output_output,
+                    output_activation,
+                )
             )
             self.w_hidden_to_output -= self.lr * d_w_hidden_2_output
             self.b_output -= self.lr * d_b_output
             self.w_input_to_hidden -= self.lr * d_w_input_2_hidden
             self.b_hidden -= self.lr * d_b_hidden
-            if (iter % 100):
+            if iter % 100:
                 loss = cross_entropy_loss(output_activation, labels)
                 print(f"Loss: {loss} for iteration {iter}")
 
 
 if __name__ == "__main__":
     training_data = np.array([[0, 0], [1, 1], [0, 1], [1, 0]])
-    labels = np.array([0, 0, 1, 1])
-    mlp = MLP(epochs=1000)
+    labels = np.array([[0], [0], [1], [1]])
+    mlp = MLP(epochs=10000)
     mlp.train(training_data, labels)
-    print(mlp.forw_pass(np.array([0,1])))
-
+    for input in training_data:
+        res = mlp.forw_pass(np.array([input]))[3]
+        print(f"{input} = {res}")
+    # print(mlp.forw_pass(np.array([0, 1])))
